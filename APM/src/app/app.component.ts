b58d4fc76889e71d5da2814331951b2fb2b05fe0
Val Conrad <vconrad@maximtrak.com>
2021-02-15 20:24:23 -0500

    Products list with images shown hidden.

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  template: `<div><h1>{{pageTitle}}</h1>
+                <pm-products></pm-products>
+              </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
